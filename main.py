89f76f2b15bf3c09f225846f0c3b3fe8f04df355
HenningRixen <henning.rixen@freenet.de>
2024-03-11 15:36:11 +0100

    laser shooting

--- main.py
+++ main.py
@@ -7,16 +7,35 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
+
+
+# backround
+backround = pygame.image.load("backround1.png")
+
 # title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# laser bullet
+laser_img = pygame.image.load("laser.png")
+laser_x = 0
+laser_y = 480
+laser_x_change = 0
+laser_y_change = 10
+laser_state = "ready"
+
+def fire_laser(x,y):
+    global laser_state
+    laser_state = "fire"
+    screen.blit(laser_img, (x + 16, y + 10))
+
 # Enemy
 enemy_img = pygame.image.load("monster.png")
 enemy_x = random.randint(0, 800)
 enemy_y = random.randint(50, 150)
-enemy_x_change = 0
+enemy_x_change = 3
+enemy_y_change = 30
 
 def enemy(x, y):
     screen.blit(enemy_img, (x, y))
@@ -37,6 +56,8 @@ while running:
     
     # RGB - Red, Green, Blue
     screen.fill((39, 45,  87))
+    # backround
+    screen.blit(backround, (0, 0))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,9 +66,13 @@ while running:
     # keystroke
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_x_change = -0.3
+                player_x_change = -5
             if event.key == pygame.K_RIGHT:
-                player_x_change = 0.3
+                player_x_change = 5
+            if event.key == pygame.K_SPACE:
+                if laser_state == "ready":
+                    laser_x = player_x
+                    fire_laser(laser_x, laser_y)
         
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -61,6 +86,29 @@ while running:
         player_x = 0
     elif player_x >= 736:
         player_x = 736
+
+    # enemy movement
+    enemy_x += enemy_x_change
+    
+    # boarders
+    if enemy_x <= 0:
+        enemy_x_change = 3
+        enemy_y += enemy_y_change
+    elif enemy_x >= 736:
+        enemy_x_change = -3
+        enemy_y += enemy_y_change
+    
+    #bullet movement
+    if laser_y <= 0:
+        laser_y = 480
+        laser_state = "ready"
+
+    if laser_state == "fire":
+        fire_laser(laser_x, laser_y)
+        laser_y -= laser_y_change
+        
+    
+
 
     # Player load 
     player(player_x, player_y)
