90bd0f70d9360f46bcbf7245d4b7ec9392dd0be7
HenningRixen <henning.rixen@freenet.de>
2024-03-11 13:36:50 +0100

    first steps

--- main.py
+++ main.py
@@ -3,4 +3,22 @@ import pygame
 # intialize the pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+#create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # RGB - Red, Green, Blue
+    screen.fill((39, 45,  87))
+    # always need that !!
+    pygame.display.update()
+
